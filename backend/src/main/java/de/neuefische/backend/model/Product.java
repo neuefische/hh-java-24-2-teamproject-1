15bbe37e61916f4c323c0f7e3a62662aaa67453b
R1d3r90 <hubertlion@aol.com>
2024-06-04 14:02:50 +0200

    add form validation

--- backend/src/main/java/de/neuefische/backend/model/Product.java
+++ backend/src/main/java/de/neuefische/backend/model/Product.java
@@ -1,6 +1,14 @@
 package de.neuefische.backend.model;
+import jakarta.validation.constraints.*;
+
 
 public record Product(
-        String id, String name, int stock, double price
+        String id,
+        @NotBlank
+        String name,
+        @Min(value = 0, message = "Stock must be greater than or equal to 0")
+        int stock,
+        @DecimalMin(value = "0.01", inclusive = false, message = "Price must be greater than 0")
+        double price
 ) {
 }
